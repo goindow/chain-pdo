b178c07d47675610b9bf4cd4ca9ef8c416444cc4
hyb <76788424@qq.com>
2020-07-09 14:09:26 +0800

    modify unit test case

--- test/testChainPDOFactory.php
+++ test/testChainPDOFactory.php
@@ -1,32 +1,52 @@
 <?php
-    require_once("../ChainPDOFactory.php");
+require_once('./punit/PunitAssert.php');
+require_once('../ChainPDOFactory.php');
 
-    $config1 = [
-        'DB_HOST' => 'localhost',
-        'DB_NAME' => 'hxcq',
-        'DB_USERNAME' => 'root',
-        'DB_PASSWORD' => '123456'
-    ];
+class TestChainPDOFactory {
 
-    $config2 = [
+    private static $config = [
         [
             'DB_HOST' => 'localhost',
-            'DB_NAME' => 'test_mybatis',
+            'DB_NAME' => 'test1',
             'DB_USERNAME' => 'root',
             'DB_PASSWORD' => '123456'
         ],[
-            'DB_HOST' => 'localhost',
-            'DB_NAME' => 'hxcq',
+            'DB_HOST' => '127.0.0.1',
+            'DB_NAME' => 'test2',
             'DB_USERNAME' => 'root',
             'DB_PASSWORD' => '123456'
         ]
     ];
 
-    $pdoFactory = ChainPDOFactory::build($config2);
-    $dbs = $pdoFactory->getDbs(); 
-    var_dump($dbs);
+    private $chainPdoFactory = null;
 
-    echo "\n";
+    public function __construct() {
+        $this->chainPdoFactory =  ChainPDOFactory::build(self::$config);
+    }
 
-    $db = $pdoFactory->getDb(0);
-    var_dump($db);
+    public function testBuild() {
+        PunitAssert::assertInstanceof($this->chainPdoFactory, 'ChainPdoFactory');
+    }
+
+    public function testGetDbs() {
+        $dbs = $this->chainPdoFactory->getDbs();
+        PunitAssert::assertEquals(count($dbs), count(self::$config));
+    }
+
+    public function testGetDdDefault() {
+        $db = $this->chainPdoFactory->getDb();
+        PunitAssert::assertInstanceof($db, 'ChainPDO');
+        PunitAssert::assertEquals($db->getConfig()['DB_NAME'], self::$config[0]['DB_NAME']);
+    }
+
+    public function testGetDdByIndex() {
+        $db = $this->chainPdoFactory->getDb(1);
+        PunitAssert::assertInstanceof($db, 'ChainPDO');
+        PunitAssert::assertEquals($db->getConfig()['DB_NAME'], self::$config[1]['DB_NAME']);
+    }
+
+    public function test() {
+        var_dump($this->chainPdoFactory);
+    }
+
+}

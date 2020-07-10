2631461d1efa3d9de2f338bb5debeeefeaaa0959
hyb <76788424@qq.com>
2020-07-10 11:45:05 +0800

    modify add assert case

--- test/punit/PunitAssert.php
+++ test/punit/PunitAssert.php
@@ -7,9 +7,11 @@ class PunitAssert {
 
     public static function assertInstanceof($obj, $class) { if (!($obj instanceof $class)) self::fail(__FUNCTION__); }
 
-    public static function assertIsString($var) { if (!is_string($var)) self::fail(__FUNCTION__); }
+    public static function assertString($var) { if (!is_string($var)) self::fail(__FUNCTION__); }
 
-    public static function assertIsInt($var) { if (!is_int($var)) self::fail(__FUNCTION__); }
+    public static function assertInt($var) { if (!is_int($var)) self::fail(__FUNCTION__); }
+
+    public static function assertGt($var1, $var2) { if (!($var1 > $var2)) self::fail(__FUNCTION__); }
 
     private static function fail($funcName) { throw new Exception("${funcName} fali."); }
 
